774d7da3a95b2e8f8105b41edf0fb7b491ac2310
sunshine0904 <sunshineboylei@gmail.com>
2018-07-25 19:44:27 +0800

    merge:add linklist struct

--- linklist/linklist.h
+++ linklist/linklist.h
@@ -8,9 +8,21 @@
 #ifndef _LINKLIST_H
 #define _LINKLIST_H
 
+struct listnode
+{
+    struct listnode *next;
+    struct listnode *prev;
+    void *data;
+};
+
 strut list
 {
+    struct listnode *head;
+    struct listnode *tail;
+    uint32_t count;
 
+    void (*cmp)(void *val1, void *val2);
+    void (*del)(void *val);
 };
 
 
